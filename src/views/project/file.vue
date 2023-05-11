2be617eb763de33a1077625dc5b6e224253f0112
“royzhz;git config --global user.name “royzhz <2937247007@qq.com>
2023-05-11 20:25:29 +0800

    美化了项目模板，实现了文件的上传和删除，js等设置，链接了项目和文件

--- src/views/project/file.vue
+++ src/views/project/file.vue
@@ -9,12 +9,13 @@
       </div>
     <div v-for="file in files" :key="file.Id" class="file-item">
       <FileRow
+        :id="id"
         :name="file.Name"
         :description="file.Project.Description"
         :updateTime="file.Updated"
-        @delete="deleteFile(file.Id)"
-        @download="downloadFile(file.Id)"
-        @rename="renameFile(file.Id)"
+        :deleteFile="deleteFile"
+        :downloadFile="downloadFile"
+        :renameFile="renameFile"
       />
     </div>
     </el-card>
@@ -24,19 +25,20 @@
 <script>
 import axios from 'axios'
 import FileRow from '@/views/project/components/FileCard/index.vue';
-import { getFile } from "@/api/project"
+import { getFile,uploadFile,deleteFile } from "@/api/project"
 export default {
   components: {
     FileRow
   },
   data() {
     return {
+      id : this.$route.params.id,
       files: []
     }
   },
   mounted() {
-    const id = this.$route.params.id;
-    this.getProjectFiles(id)
+    this.id = this.$route.params.id;
+    this.getProjectFiles(this.id)
   },
   methods: {
     getProjectFiles(id) {
@@ -45,14 +47,36 @@ export default {
         console.log(this.files)
       })
     },
-    deleteFile(id) {
+    deleteFile(id,filename) {
+      deleteFile(id,filename).then(response => {
+        this.getProjectFiles(this.id)
+      })
       // 实现删除文件的逻辑
     },
     downloadFile(id) {
       // 实现下载文件的逻辑
+
     },
     renameFile(id) {
       // 实现重命名文件的逻辑
+
+    },
+    handleCreate(){
+      const fileInput = document.createElement('input');
+      fileInput.type = 'file';
+
+      fileInput.addEventListener('change', () => {
+        const file = fileInput.files[0];
+        // 在这里可以对文件进行处理，例如发送到服务器等
+        const formData = new FormData();
+        formData.append('uploadname', file, file.name);
+        formData.append('savename', file.name);
+        console.log("uploaded")
+        uploadFile(this.id,formData).then(response => {
+          this.getProjectFiles(this.id)
+        })
+      });
+      fileInput.click();
     }
   }
 }
