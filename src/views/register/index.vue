edfc16d1864f7067739662a66edb2983ca8769db
“royzhz;git config --global user.name “royzhz <2937247007@qq.com>
2023-05-11 12:27:35 +0800

    注册功能实现

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -1,56 +1,73 @@
 <template>
   <div class="auth-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="auth-form" auto-complete="on"
+    <el-form ref="registerForm" :model="registerForm" :rules="registerRules" class="auth-form" auto-complete="on"
              label-position="left">
       <div class="wrapper">
         <div class="title-container">
           <img
             src="https://user-images.githubusercontent.com/75596353/236753989-c95dd9d6-029e-4456-aec4-dd65363a9c5d.png"
             alt="PPTCopilot Logo" class="logo">
-          <h1 class="title">登录</h1>
+          <h1 class="title">注册</h1>
         </div>
 
-        <el-form-item prop="username_or_email">
+        <el-form-item prop="username">
         <span class="svg-container">
           <svg-icon icon-class="user"/>
         </span>
-          <el-input ref="username_or_email" v-model="loginForm.username_or_email" placeholder="Username or Email"
-                    name="username_or_email" type="text" tabindex="1" auto-complete="on"/>
+          <el-input ref="username" v-model="registerForm.username" placeholder="Username"
+                    name="username" type="text" tabindex="1" auto-complete="on"/>
+        </el-form-item>
+
+        <el-form-item prop="email">
+        <span class="svg-container">
+          <svg-icon icon-class="email"/>
+        </span>
+          <el-input ref="email" v-model="registerForm.email" placeholder="Email"
+                    name="email" type="text" tabindex="2" auto-complete="on"/>
         </el-form-item>
 
         <el-form-item prop="password">
         <span class="svg-container">
           <svg-icon icon-class="password"/>
         </span>
-          <el-input :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType"
-                    placeholder="Password" name="password" tabindex="2" auto-complete="on"
-                    @keyup.enter.native="handleLogin"/>
+          <el-input :key="passwordType" ref="password" v-model="registerForm.password" :type="passwordType"
+                    placeholder="Password" name="password" tabindex="3" auto-complete="on"
+                    @keyup.enter.native="handleRegister"/>
           <span class="show-pwd" @click="showPwd">
           <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"/>
         </span>
         </el-form-item>
 
         <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;"
-                   @click.native.prevent="handleLogin">登录
+                   @click.native.prevent="handleRegister">注册
         </el-button>
 
         <div class="auth-footer">
-          <span>没有账号？</span>
-          <router-link to="/register">注册</router-link>
+          <span>已经有账号？</span>
+          <router-link to="/login">登录</router-link>
         </div>
       </div>
     </el-form>
   </div>
 </template>
-
 <script>
+import {register} from "@/api/user"
 
 export default {
-  name: 'Login',
+  name: 'Register',
+
   data() {
     const validateUsername = (rule, value, callback) => {
       if (value.length < 4) {
         callback(new Error('The username can not be less than 4 digits'))
+      } else {
+        callback()
+      }
+    }
+    const validateEmail = (rule, value, callback) => {
+      const emailRegEx = /\S+@\S+\.\S+/;
+      if (!emailRegEx.test(value)) {
+        callback(new Error('Please input a valid email address'))
       } else {
         callback()
       }
@@ -63,25 +80,18 @@ export default {
       }
     }
     return {
-      loginForm: {
-        username_or_email: 'john_doe',
-        password: 'password123'
+      registerForm: {
+        username: '',
+        email: '',
+        password: ''
       },
-      loginRules: {
-        username_or_email: [{required: true, trigger: 'blur', validator: validateUsername}],
+      registerRules: {
+        username: [{required: true, trigger: 'blur', validator: validateUsername}],
+        email: [{required: true, trigger: 'blur', validator: validateEmail}],
         password: [{required: true, trigger: 'blur', validator: validatePassword}]
       },
       loading: false,
       passwordType: 'password',
-      redirect: undefined
-    }
-  },
-  watch: {
-    $route: {
-      handler: function (route) {
-        this.redirect = route.query && route.query.redirect
-      },
-      immediate: true
     }
   },
   methods: {
@@ -95,14 +105,20 @@ export default {
         this.$refs.password.focus()
       })
     },
-    handleLogin() {
+    handleRegister() {
       this.loading = true
-      this.$store.dispatch('user/login', this.loginForm).then(() => {
-        this.$router.push({path: this.redirect || '/'})
-        this.loading = false
-      }).catch(() => {
-        this.loading = false
-      })
+      register(this.registerForm)
+        .then(() => {
+          this.$router.push({path: '/login'})
+          this.loading = false
+          this.$message({
+            type: 'success',
+            message: '注册成功,请登陆'
+          })
+        })
+        .catch(() => {
+          this.loading = false
+        })
     }
   }
 }
@@ -110,10 +126,12 @@ export default {
 
 <style lang="scss">
 @import "@/assets/css/auth.scss";
+
 @include authInput();
 </style>
 
 <style lang="scss" scoped>
 @import "@/assets/css/auth.scss";
+
 @include authBackground();
 </style>
